3e7dc69f577cd5662ebcf391981f9581141b2513
Ray Naldo <raynaldo@unpar.ac.id>
2017-05-03 08:08:28 +0700

    Props

--- index.android.js
+++ index.android.js
@@ -7,16 +7,34 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Text, TextInput,
-  ScrollView
+  Text, View, Image
 } from 'react-native';
 
 
+class Greeting extends Component 
+{
+  render ()
+  {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
 export default class HelloReactApp extends Component 
 {
-	render() {
+	render () 
+	{
+		let pic = {
+		  uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+		};
 		return (
-		  <Text>Hello world!</Text>
+			<View style={{alignItems: 'center'}}>
+				<Greeting name='Rexxar' />
+				<Greeting name='Jaina' />
+				<Greeting name='Valeera' />
+				<Image source={pic} style={{width: 193, height: 110}}/>
+			</View>
 		);
 	}
 }
