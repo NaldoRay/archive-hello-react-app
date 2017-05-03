3d44fd8b7dd034efdc89550f40f4746980a1b6d1
Ray Naldo <raynaldo@unpar.ac.id>
2017-05-03 08:21:39 +0700

    Getting Started: Handling Text Input

--- index.android.js
+++ index.android.js
@@ -6,27 +6,31 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry, View
+  AppRegistry, 
+  View, Text, TextInput
 } from 'react-native';
 
 
 export default class HelloReactApp extends Component 
 {
-	render () 
+	constructor(props) 
+	{
+		super(props);
+		this.state = {text: ''};
+	}
+
+	render() 
 	{
 		return (
-			// Try setting `alignItems` to 'flex-start'
-			// Try setting `justifyContent` to `flex-end`.
-			// Try setting `flexDirection` to `row`.
-			<View style={{
-				flex: 1,
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-				<View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-				<View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-				<View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+			<View style={{padding: 10}}>
+				<TextInput
+					style={{height: 40}}
+					placeholder="Type here to translate!"
+					onChangeText={(text) => this.setState({text})}
+				/>
+				<Text style={{padding: 10, fontSize: 42}}>
+					{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+				</Text>
 			</View>
 		);
 	}
